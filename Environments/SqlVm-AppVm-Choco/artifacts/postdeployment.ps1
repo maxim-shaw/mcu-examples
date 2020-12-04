fb1cacae093502c82a2cd434400505b621bef9d3
garaus <maxim.shaw@hotmail.com>
2020-12-04 08:39:07 -0500

    Choco install script

--- Environments/SqlVm-AppVm-Choco/artifacts/postdeployment.ps1
+++ Environments/SqlVm-AppVm-Choco/artifacts/postdeployment.ps1
@@ -6,7 +6,7 @@ Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Chocolatey packages 
-choco install paint.net -y
+choco install dotnetcore-sdk -y
 choco install notepadplusplus -y
 
 # Reboot
