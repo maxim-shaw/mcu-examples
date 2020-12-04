922a064ee781d211915d5ba9cec34c42b2fa011f
garaus <maxim.shaw@hotmail.com>
2020-12-03 23:29:35 -0500

    Choco install script

--- Environments/SqlVm-AppVm-Choco/artifacts/chocopackages.ps1
+++ Environments/SqlVm-AppVm-Choco/artifacts/chocopackages.ps1
@@ -1,9 +1,9 @@
-// Install Chocolatey 
+# Install Chocolatey 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-// Install Chocolatey packages 
-choco install paint.net
-choco install notepadplusplus
+# Install Chocolatey packages 
+choco install paint.net -y
+choco install notepadplusplus -y
 
 # Reboot
-//Restart-Computer
+# Restart-Computer
