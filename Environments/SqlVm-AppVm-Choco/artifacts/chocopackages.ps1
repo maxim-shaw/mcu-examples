78ad79ad12190ab91b7a19fe7f9f37d8dee56f9a
garaus <maxim.shaw@hotmail.com>
2020-12-04 00:21:35 -0500

    Choco install script

--- Environments/SqlVm-AppVm-Choco/artifacts/chocopackages.ps1
+++ Environments/SqlVm-AppVm-Choco/artifacts/chocopackages.ps1
@@ -1,3 +1,6 @@
+# Install IIS
+Install-WindowsFeature -name Web-Server -IncludeManagementTools && powershell.exe remove-item 'C:\\inetpub\\wwwroot\\iisstart.htm' && powershell.exe Add-Content -Path 'C:\\inetpub\\wwwroot\\iisstart.htm' -Value $('Hello World from ' + $env:computername)
+
 # Install Chocolatey 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
