f872ea35af461a4d973af0939603db1561cfdeeb
garaus <maxim.shaw@hotmail.com>
2020-12-11 11:17:48 -0500

    Domain join dependency for post-deployment scripts added

---
+++ Environments/SqlVm-LinkedTemplate/scripts/apppostdeployment.ps1
@@ -0,0 +1,13 @@
+# Install IIS
+#Install-WindowsFeature -name Web-Server -IncludeManagementTools && powershell.exe remove-item 'C:\\inetpub\\wwwroot\\iisstart.htm' && powershell.exe Add-Content -Path 'C:\\inetpub\\wwwroot\\iisstart.htm' -Value $('Hello World from ' + $env:computername)
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+# Install Chocolatey 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Chocolatey packages 
+choco install dotnetcore-sdk -y
+choco install notepadplusplus -y
+
+# Reboot
+Restart-Computer
