00d2148635429d24db4d889ae246080e4d157c01
garaus <maxim.shaw@hotmail.com>
2020-12-11 09:57:10 -0500

    New base iamge SQLSP2 test

--- Environments/SqlVm-AppVm-Choco/artifacts/dbpostdeployment.ps1
+++ Environments/SqlVm-AppVm-Choco/artifacts/dbpostdeployment.ps1
@@ -1,5 +1,5 @@
 # Format and initialize the data Disk
-Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
+Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
 
 # Reboot
 Restart-Computer
